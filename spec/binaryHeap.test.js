d7f5cea11c7ebafd3584e8693df1ed9317958080
Hao-Han Pan <c.avantin94@gmail.com>
2019-01-22 21:33:02 -0600

    Validate min heap creation

---
+++ spec/binaryHeap.test.js
@@ -0,0 +1,35 @@
+const BinaryHeap = require('../data-structures/BinaryHeap.js');
+
+test('should create a min heap when initialized with no comparator', () => {
+  const heap = new BinaryHeap();
+  heap.insert(generateRandomArray(10))
+  console.log(heap);
+  expect(isValidMinHeap(heap._heap)).toBe(true);
+});
+
+/* Helpers */
+
+const isValidMinHeap = function(heap) {
+  if (!heap.length) {
+    return false;
+  }
+  for (var i = heap.length - 1; i >= 0; i--) {
+    const parent = Math.floor((i - 1) / 2)
+    if (heap[parent] > heap[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const generateRandomArray = function(length) {
+  const result = [];
+  for (var i = 0; i < length; i++) {
+    result.push(generateRandomInt());
+  }
+  return result;
+};
+
+const generateRandomInt = function() {
+  return Math.floor(Math.random() * 50);
+};

a0959c5774a88d0fe88f8122244d308124e10556
manifold-menagerie <manifold.menagerie@gmail.com>
2019-01-25 00:58:00 +0200

    Add heap validator

--- helpers/validators.js
+++ helpers/validators.js
@@ -4,8 +4,26 @@ const isSorted = function(array, comparator) {
   if (typeof comparator !== 'function') {
     comparator = function(a, b) { return a < b };
   }
+  
   for (var i = 0; i < array.length - 1; i++) {
     if (!comparator(array[i], array[i + 1]) && comparator(array[i + 1], array[i])) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const isValidHeap = function(heap, comparator) {
+  if (!heap.length) {
+    return false;
+  }
+  if (typeof comparator !== 'function') {
+    comparator = function(a, b) { return a < b };
+  }
+  
+  for (var i = heap.length - 1; i >= 0; i--) {
+    const parent = Math.floor((i - 1) / 2)
+    if (!comparator(heap[parent], heap[i]) && comparator(heap[i], heap[parent])) {
       return false;
     }
   }

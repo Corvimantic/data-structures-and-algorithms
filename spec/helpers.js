8800a2c23802ebac204b7e31b463f57af5792a69
Hao-Han Pan <c.avantin94@gmail.com>
2019-01-23 22:45:26 -0600

    Add isSorted helper

--- spec/helpers.js
+++ spec/helpers.js
@@ -46,10 +46,25 @@ const incrementCounter = function(counter, value) {
   }
 }
 
+// Optional: pass in a comparator to use for validation.
+// Default checks to see if sorted from smallest to largest.
+const isSorted = function(array, comparator) {
+  if (!comparator) {
+    comparator = function(a, b) { return a < b };
+  }
+  for (var i = 0; i < array.length - 1; i++) {
+    if (!comparator(array[i], array[i + 1]) && comparator(array[i + 1], array[i])) {
+      return false;
+    }
+  }
+  return true;
+};
+
 /* Exports */
 
 module.exports = {
   generateRandomArray: generateRandomArray,
   generateRandomInt: generateRandomInt,
   generateRandomObjectArray: generateRandomObjectArray,
+  isSorted: isSorted,
 };

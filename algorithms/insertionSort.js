88bfa24aa2afba09e70db8b74515353c32370271
Hao-Han Pan <c.avantin94@gmail.com>
2019-01-23 20:17:35 -0600

    Add an insertion sort algorithm

---
+++ algorithms/insertionSort.js
@@ -0,0 +1,27 @@
+/* ***
+Optional: pass in a function to use as a comparator.
+If no comparator is specified, the function will default to sorting least to greatest.
+This algorithm sorts an array in place and WILL mutate the input array.
+*** */
+const insertionSort = function(array, comparator) {
+  if (!comparator) {
+    comparator = function(a, b) { return a < b };
+  }
+
+  for (var i = 1; i < array.length; i++) {
+    const item = array.splice(i, 1)[0];
+
+    for (var j = 0; j < i + 1; j++) {
+      if (j === i) {
+        array.splice(j, 0, item);
+      }
+      if (comparator(item, array[j])) {
+        array.splice(j, 0, item);
+        break;
+      }
+    }
+  }
+  return array;
+};
+
+module.exports = insertionSort;

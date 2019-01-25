0bb212d63e36765f040683ca4366800d02b1b126
manifold-menagerie <manifold.menagerie@gmail.com>
2019-01-25 05:35:18 +0200

    Rearrange tests for clarity

--- spec/algorithms.test.js
+++ spec/algorithms.test.js
@@ -8,34 +8,11 @@ describe('insertionSort', () => {
     expect(sortedArray).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
   });
   
-  test('should sort duplicate values', () => {
-    const array = [3, 7, 8, 3, 4, 7, 8, 8, 9, 0];
-    const sortedArray = insertionSort(array);
-
-    expect(sortedArray).toEqual([0, 3, 3, 4, 7, 7, 8, 8, 8, 9]);
-  });
-  
   test('should sort non-integers', () => {
     const array = ['b', 'h', 'a', 'e', 'j', 'd', 'i', 'c', 'g', 'f'];
     const sortedArray = insertionSort(array);
 
     expect(sortedArray).toEqual(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']);
-  });
-  
-  test('should return the same empty array passed in when given an empty array', () => {
-    const array = [];
-    const sortedArray = insertionSort(array);
-
-    expect(sortedArray).toEqual([]);
-    expect(sortedArray).toBe(array);
-  });
-  
-  test('should return the same single-entry array passed in when given a single-entry array', () => {
-    const array = [0];
-    const sortedArray = insertionSort(array);
-
-    expect(sortedArray).toEqual([0]);
-    expect(sortedArray).toBe(array);
   });
 
   test('should sort using a comparator', () => { 
@@ -45,12 +22,12 @@ describe('insertionSort', () => {
 
     expect(sortedArray).toEqual([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]);
   });
-
-  test('should mutate the input array', () => {
-    const array = [3, 7, 8, 1, 9, 4, 6, 5, 2, 0];
+  
+  test('should sort duplicate values', () => {
+    const array = [3, 7, 8, 3, 4, 7, 8, 8, 9, 0];
     const sortedArray = insertionSort(array);
 
-    expect(sortedArray).toBe(array);
+    expect(sortedArray).toEqual([0, 3, 3, 4, 7, 7, 8, 8, 8, 9]);
   });
 
   test('should be a stable sort', () => { 
@@ -71,5 +48,28 @@ describe('insertionSort', () => {
       {value: 1, order: 2}, 
       {value: 1, order: 3}
     ]); 
+  });
+
+  test('should mutate the input array', () => {
+    const array = [3, 7, 8, 1, 9, 4, 6, 5, 2, 0];
+    const sortedArray = insertionSort(array);
+
+    expect(sortedArray).toBe(array);
+  });
+  
+  test('should return the same empty array passed in when given an empty array', () => {
+    const array = [];
+    const sortedArray = insertionSort(array);
+
+    expect(sortedArray).toEqual([]);
+    expect(sortedArray).toBe(array);
+  });
+  
+  test('should return the same single-entry array passed in when given a single-entry array', () => {
+    const array = [0];
+    const sortedArray = insertionSort(array);
+
+    expect(sortedArray).toEqual([0]);
+    expect(sortedArray).toBe(array);
   });
 }); 

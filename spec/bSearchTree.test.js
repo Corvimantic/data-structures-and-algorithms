e5b12d6f3329482a8978923ee30f7f066f66f15f
Hao-Han Pan <c.avantin94@gmail.com>
2019-01-29 23:59:42 -0600

    Add spec stubs for bSearchTree

---
+++ spec/bSearchTree.test.js
@@ -0,0 +1,39 @@
+const bSearchTree = require('../data-structures/bSearchTree.js');
+
+describe('bSearchTree', () => {
+  test('should create an instance with a node of the provided value', () => {
+    // FILL ME IN
+  });
+
+  test('should insert new nodes with .insert', () => {
+    // FILL ME IN
+  });
+
+  test('should remove leaf nodes with .delete', () => {
+    // FILL ME IN
+  });
+
+  test('should remove non-leaf nodes with .delete', () => {
+    // FILL ME IN
+  });
+
+  test('should .delete with a comparator', () => {
+    // FILL ME IN
+  });
+
+  test('should return the deleted value with .delete', () => {
+    // FILL ME IN
+  });
+
+  test('should return true with .find when given an existent value and not given a comparator', () => {
+    // FILL ME IN
+  });
+
+  test('should .find with a comparator', () => {
+    // FILL ME IN
+  });
+
+  test('should return undefined with .find when given a non-existent value', () => {
+    // FILL ME IN
+  });
+});

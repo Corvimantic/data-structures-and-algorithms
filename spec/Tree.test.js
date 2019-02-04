c04f809bd7a36fa464673fd74c565678d6cab2b1
Hao-Han Pan <c.avantin94@gmail.com>
2019-02-03 19:46:54 -0600

    Create basic specs for Tree

---
+++ spec/Tree.test.js
@@ -0,0 +1,14 @@
+const Tree = require('../data-structures/Tree.js');
+
+describe('Tree', () => {
+  test('should create an instance with a node of the provided value', () => {
+    const tree = new Tree(0);
+    expect(tree.value).toBe(0);
+  });
+
+  test('should add a child with .addChild', () => {
+    const tree = new Tree(0);
+    tree.addChild(1);
+    expect(tree.children[0].value).toEqual({ value: 1, children: [] });
+  });
+});
